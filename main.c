6c0bae28159ebf9d7ca66e158156ec8da34cb861
Andrew Le <andrewlkm@spgroup.com.sg>
2020-05-26 12:04:45 +0800

    Fahrenheit to Celsius

--- main.c
+++ main.c
@@ -2,5 +2,23 @@
 
 int main() {
     printf("Hello, World!\n");
+
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    printf("Fahr\tCelsius\n");
+    while (fahr <= upper) {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+
+        fahr += step;
+    }
+
     return 0;
 }
