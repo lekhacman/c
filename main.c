9461efecaee4a89b61ac2781ef284b912cf3acd3
Andrew Le <andrewlkm@spgroup.com.sg>
2020-05-26 13:02:32 +0800

    Fahrenheit to Celsius and vice versa

--- main.c
+++ main.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 
+float fToC(float fahr);
+float cToF(float celsius);
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main() {
     printf("Hello, World!\n");
 
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
 
-    fahr = lower;
+    fahr = LOWER;
 
     printf("Fahr\tCelsius\n");
-    while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+    while (fahr <= UPPER) {
+        celsius = fToC(fahr);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
 
-        fahr += step;
+        fahr += STEP;
+    }
+
+    printf("Celsius\tFahr\n");
+    for (float i = 0; i < UPPER; i += STEP) {
+        printf("%3.0f\t%6.1f\n", i, cToF(i));
     }
 
     return 0;
+}
+
+float fToC(float fahr) {
+    return 5.0 * (fahr - 32) / 9.0
+}
+
+float cToF(float celsius) {
+    return celsius * 9.0 / 5.0 + 32;
 }
